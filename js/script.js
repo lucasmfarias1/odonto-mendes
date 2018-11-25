27f117a892b03ca8dd6067dce8bb7a95e75507ce
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-11-25 18:24:01 -0200

    adicionar botao de aumentar a fonte

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var fonteGrande = false;
+
+$("#aumentar-fonte").click(function() {
+  if (fonteGrande) {
+    $("body").css("font-size", "100%");
+    $("#mp").css("padding", "14px 0");
+    $(this).css("background", "#fff");
+    fonteGrande = false;
+  } else {
+    $("body").css("font-size", "125%");
+    $("#mp").css("padding", "10px 0");
+    $("#linha3").css("height", "350px");
+    $(this).css("background", "#ccc");
+    fonteGrande = true;
+  }
+});
+
+$("#diminuir-fonte").click(function() {
+});
+
+
+// ul#mp {
+//   margin: 2px 0 0 0;
+//   padding: 14px 0;
+//   font-size: 1.2em;
+// }
